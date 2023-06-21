679f63dd2ca10078655b6c796e7a431a778f1035
Henrik Schnettler <henrik@schnettler.dev>
2023-06-21 21:31:41 +0200

    Implemented MusicKitManager for authorization. Realized that developer token and user token is managed by the application itself so i don't need to fetch the devtoken from my api anymore. Implemented logic to only show the main view if initial authorization was made and user gave apple music permission

--- MusicMate/Classes/MusicKitManager.swift
+++ MusicMate/Classes/MusicKitManager.swift
@@ -8,67 +8,51 @@
 import Foundation
 import MusicKit
 import StoreKit
+import Combine
 
-class MusicKitManager {
-    private let keychainService = KeychainService()
-    private let developerTokenKey = "MUSICKIT_API_DEVTOKEN"
-    private let apiToken = Bundle.main.object(forInfoDictionaryKey: "NODEJS_API_TOKEN") as? String
-    private let controller = SKCloudServiceController()
+class MusicKitManager: ObservableObject {
+    //reference to the singleton is reachable over the static constant of the class
+    static let shared = MusicKitManager()
     
-    func performFullAuthorization() -> Void {
+    private init() {
+        // full authorization is made on init
+        self.performInitialAuthorization()
+    }
+    
+    //Keychain service in initalized
+    private let keychainService = KeychainService()
+    //The key in the keychain where the developer token should be saved
+    private let developerTokenKey = "MUSICKIT_API_DEVTOKEN"
+    //The token for the musicmate api is retrieved form the xcconfig file
+    private let apiToken = Bundle.main.object(forInfoDictionaryKey: "NODEJS_API_TOKEN") as? String
+    
+    //The following variables notify the view if they update to it can refresh
+    //@Published var authorizationStatus = SKCloudServiceAuthorizationStatus.notDetermined
+    @Published var activeAppleMusicMembership = false
+    @Published var initalAuthentificationComplete = false
+    @Published var appleMusicAccessGrantedByUser = false
+    
+    private func performInitialAuthorization() -> Void {
         self.checkAppleMusicAccess() { accessIsGranted in
-            if(accessIsGranted)
+            if(!accessIsGranted)
             {
-                self.checkUserCapabilities(){capability, error in
-                    if(self.isDeveloperTokenStored())
+                //If access isnt granted the user is asked to do it
+                self.requestAppleMusicAccess() { grantSuccessfull in
+                    if(!grantSuccessfull)
                     {
-                        //if the developer token is avaiable the user token should be reuqested to check if the developerToken is still valid
-                        self.requestUserToken(){userToken, error in
-                            if(error != nil)
-                            {
-                                //if there is a error with the rquest the developer token should be requested once again
-                                self.fetchDeveloperToken(){ devtoken, error in
-                                }
-                            }
-                        }
+                        //The user should be alerted that the app only works with apple music access
                     }
                     else{
-                        //if the developer token isnt avaible in keychain it should be requested from the musicmate api
-                        self.fetchDeveloperToken(){ devtoken, error in
+                        self.checkUserCapabilities(){capability, error in
                         }
                     }
                 }
             }
             else{
-                //If access isnt granted the user is asked to do it
-                self.requestAppleMusicAccess() { grantSuccessfull in
-                    if(grantSuccessfull)
-                    {
-                        self.checkUserCapabilities(){capability, error in
-                            if(self.isDeveloperTokenStored())
-                            {
-                                //if the developer token is avaiable the user token should be reuqested to check if the developerToken is still valid
-                                self.requestUserToken(){userToken, error in
-                                    if(error != nil)
-                                    {
-                                        //if there is a error with the rquest the developer token should be requested once again
-                                        self.fetchDeveloperToken(){ devtoken, error in
-                                        }
-                                    }
-                                }
-                            }
-                            else{
-                                //if the developer token isnt avaible in keychain it should be requested from the musicmate api
-                                self.fetchDeveloperToken(){ devtoken, error in
-                                }
-                            }
-                        }
-                    }
-                    else{
-                        //The user should be alerted that the app only works with apple music access
-                    }
+                self.checkUserCapabilities(){capability, error in
                 }
             }
+            self.initalAuthentificationComplete = true
         }
     }
     
@@ -78,15 +62,19 @@ class MusicKitManager {
         switch status {
             case .authorized:
                 print("Access to Apple Music granted.")
+                self.appleMusicAccessGrantedByUser = true
                 completion(true)
             case .denied, .restricted:
                 print("Access to Apple Music denied or restricted.")
+                self.appleMusicAccessGrantedByUser = false
                 completion(false)
             case .notDetermined:
                 print("Access to Apple Music not determined.")
+                self.appleMusicAccessGrantedByUser = false
                 completion(false)
             @unknown default:
                 print("Unknown authorization status for Apple Music.")
+                self.appleMusicAccessGrantedByUser = false
                 completion(false)
         }
     }
@@ -96,16 +84,18 @@ class MusicKitManager {
             switch status {
                 case .authorized:
                     print("authorized")
+                    self.appleMusicAccessGrantedByUser = true
                     completion(true)
                 default:
                     print("authorization denied or restricted")
+                    self.appleMusicAccessGrantedByUser = false
                     completion(false)
             }
         }
     }
     
     private func checkUserCapabilities(completion: @escaping (SKCloudServiceCapability,Error?) -> Void) {
-        self.controller.requestCapabilities { (capability, error) in
+        SKCloudServiceController().requestCapabilities { (capability, error) in
                 if let error = error {
                     print("An error occurred when requesting capabilities: \(error.localizedDescription)")
                     return
@@ -116,75 +106,13 @@ class MusicKitManager {
                 switch capability {
                 case .musicCatalogPlayback:
                     defaults.set(true, forKey: "activeAppleMusicMembership")
+                    self.activeAppleMusicMembership = true
                 default:
                     defaults.set(false, forKey: "activeAppleMusicMembership")
+                    self.activeAppleMusicMembership = false
                 }
                 
                 completion(capability, error)
         }
-    }
-
-    private func requestUserToken(completion: @escaping (String?, Error?) -> Void) {
-        let developerToken = self.getDeveloperTokenFromKeychain() ?? ""
-        
-        self.controller.requestUserToken(forDeveloperToken: developerToken) { (userToken, error) in
-                if let error = error {
-                    print("An error occurred when requesting the user token: \(error)")
-                    
-                    let nsError = error as NSError
-                    if nsError.domain == SKErrorDomain && nsError.code == SKError.cloudServiceNetworkConnectionFailed.rawValue {
-                        print("The developer token may be expired or invalid.")
-                        completion(nil, nsError)
-                    }
-                } else if let userToken = userToken {
-                    completion(userToken, nil)
-                }
-            }
-    }
-
-    private func fetchDeveloperToken(completion: @escaping (String?, Error?) -> Void) {
-        let url = URL(string: "https://api.musicmate.schnettler.dev/token")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue(self.apiToken!, forHTTPHeaderField: "authorization")
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-            } else if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-
-                    if let jsonDict = json as? [String: Any], let developerToken = jsonDict["developerToken"] as? String {
-                        self.keychainService.save(key: self.developerTokenKey, value: developerToken)
-                        completion(developerToken, nil)
-                    } else {
-                        let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Received unexpected JSON format"])
-                        completion(nil, error)
-                    }
-                } catch {
-                    let developerToken = String(decoding: data, as: UTF8.self)
-                    self.keychainService.save(key: self.developerTokenKey, value: developerToken)
-                    completion(developerToken, nil)
-                }
-            }
-        }
-
-        task.resume()
-    }
-
-    
-    private func isDeveloperTokenStored() -> Bool {
-        if(self.getDeveloperTokenFromKeychain() == nil)
-        {
-            return false
-        }
-        else{
-            return true
-        }
-    }
-    
-    private func getDeveloperTokenFromKeychain() -> String? {
-        return keychainService.get(key: developerTokenKey)
     }
 }
--- MusicMate/MusicMateApp.swift
+++ MusicMate/MusicMateApp.swift
@@ -10,16 +10,27 @@ import SwiftUI
 @main
 struct MusicMateApp: App {
     let persistenceController = PersistenceController.shared
-    let musicKitManager = MusicKitManager()
+    //reference to the singleton instance of MusicKitManager class
+    @StateObject private var musicKitManager = MusicKitManager.shared
 
     var body: some Scene {
         WindowGroup {
-            MainView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                .onAppear(){
-                    //every time the app is started full authorization should be made
-                    musicKitManager.performFullAuthorization()
+            if musicKitManager.initalAuthentificationComplete{
+                //initial authentification is complete after start of the app
+                if musicKitManager.appleMusicAccessGrantedByUser{
+                    //user gave permission to access apple music so MainView can be shown
+                    MainView()
+                        .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                        .onAppear(){
+                        }
                 }
+                else{
+                    //user didnt gave permission so he cant access functionality of the app. He should be notified that he needs to give access for the app to work. The app could close after that.
+                }
+            }
+            else{
+                //if initial authentification isnt made yet MainView shouldnt be visible instead some sort of loadin screen/splash screen
+            }
         }
     }
 }
