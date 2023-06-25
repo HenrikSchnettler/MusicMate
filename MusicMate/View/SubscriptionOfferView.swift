42ed28f04b7bb51fa9586d9ecc48271173a6fc48
Henrik Schnettler <106490104+HenrikSchnettler@users.noreply.github.com>
2023-06-25 23:27:22 +0200

    Add files via upload
    added views which are shown by different authetification outcomes

---
+++ MusicMate/View/SubscriptionOfferView.swift
@@ -0,0 +1,36 @@
+//
+//  SubscriptionOfferView.swift
+//  MusicMate
+//
+//  Created by Henrik Schnettler on 25.06.23.
+//
+
+import SwiftUI
+import MusicKit
+
+struct SubscriptionOfferView: View {
+    @EnvironmentObject var musicKitManager: MusicKitManager
+    @State var isShowingOffer = false
+
+    var offerOptions: MusicSubscriptionOffer.Options {
+        var offerOptions = MusicSubscriptionOffer.Options()
+        //offerOptions.itemID = album.id
+        return offerOptions
+    }
+
+    var body: some View {
+        Button("Show Subscription Offers", action: showSubscriptionOffer)
+            .disabled(!(musicKitManager.musicSubscription?.canBecomeSubscriber ?? false))
+            .musicSubscriptionOffer(isPresented: $isShowingOffer, options: offerOptions)
+    }
+
+    func showSubscriptionOffer() {
+        isShowingOffer = true
+    }
+}
+
+struct SubscriptionOfferView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubscriptionOfferView()
+    }
+}
