f647f377c98c6631d0cdbaf776e3cbd77a3c65e8
Henrik Schnettler <henrik@schnettler.dev>
2023-09-11 18:17:40 +0200

    comments for integrated UiKit View Components

--- MusicMate/View/UIView/VideoPlayerView.swift
+++ MusicMate/View/UIView/VideoPlayerView.swift
@@ -9,10 +9,16 @@ import Foundation
 import AVKit
 import SwiftUI
 
+/// A SwiftUI view to display a video player using a UIKit UIView.
 struct VideoPlayerView: UIViewRepresentable {
+    
+    // The URL of the video to be played.
     var url: URL
+    
+    // A flag to indicate whether the video should be playing or paused.
     var isActive: Bool
     
+    /// Create the UIView instance for the SwiftUI view.
     func makeUIView(context: Context) -> UIView {
         let view = VideoPlayerUiView(frame: .zero)
         view.isActive = isActive
@@ -20,6 +26,7 @@ struct VideoPlayerView: UIViewRepresentable {
         return view
     }
     
+    /// Update the created UIView instance with any changes in SwiftUI view's state.
     func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<VideoPlayerView>) {
         if let view = uiView as? VideoPlayerUiView {
             view.isActive = isActive
@@ -27,10 +34,16 @@ struct VideoPlayerView: UIViewRepresentable {
     }
 }
 
+/// A custom UIView to play a video using AVKit.
 class VideoPlayerUiView: UIView {
+    
+    // AVPlayerLayer instance to render the video content.
     private var playerLayer = AVPlayerLayer()
+    
+    // A looper instance to loop the video playback.
     private var looper: AVPlayerLooper?
     
+    // A computed property to manage the AVQueuePlayer associated with the playerLayer.
     var videoPlayer: AVQueuePlayer? {
         get {
             return playerLayer.player as? AVQueuePlayer
@@ -40,8 +53,10 @@ class VideoPlayerUiView: UIView {
         }
     }
     
+    // A property to monitor the video's playback status.
     var isActive: Bool = false {
         didSet {
+            // Play the video when isActive is true, otherwise, pause and reset the video.
             if isActive {
                 videoPlayer?.play()
             } else {
@@ -53,7 +68,11 @@ class VideoPlayerUiView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        // Set the video's display mode.
         playerLayer.videoGravity = .resizeAspectFill
+        
+        // Add the playerLayer to the UIView's layer.
         layer.addSublayer(playerLayer)
     }
     
@@ -61,15 +80,23 @@ class VideoPlayerUiView: UIView {
         super.init(coder: coder)
     }
     
+    /// Layout the playerLayer to match the UIView's bounds.
     override func layoutSubviews() {
         super.layoutSubviews()
         playerLayer.frame = bounds
     }
     
+    /// Set up the video for looped playback.
     func playInLoop(url: URL) {
         let playerItem = AVPlayerItem(url: url)
+        
+        // Initialize the AVQueuePlayer and set it to the videoPlayer.
         videoPlayer = AVQueuePlayer(playerItem: playerItem)
+        
+        // Create a loop for the video playback.
         looper = AVPlayerLooper(player: videoPlayer!, templateItem: playerItem)
+        
+        // If isActive is true, play the video. Otherwise, reset its position.
         if isActive {
             videoPlayer?.play()
         } else {
--- MusicMate/View/UIView/VisualEffectView.swift
+++ MusicMate/View/UIView/VisualEffectView.swift
@@ -7,8 +7,22 @@
 
 import SwiftUI
 
+// A SwiftUI view that represents a UIVisualEffectView from UIKit to display visual effects.
 struct VisualEffectView: UIViewRepresentable {
+
+    // The visual effect to be applied. It can be `nil` to represent no effect.
     var effect: UIVisualEffect?
-    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
-    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+    
+    /// Creates the UIVisualEffectView for use in SwiftUI.
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+        // Initializes a new instance of UIVisualEffectView.
+        return UIVisualEffectView()
+    }
+    
+    /// Updates the created UIVisualEffectView instance with the specified effect.
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+        // Apply the provided visual effect to the UIVisualEffectView.
+        uiView.effect = effect
+    }
 }
+
